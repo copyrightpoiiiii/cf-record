18d366c2e92de81eaee618ee63a385b9cc192ee1
copyrightpoiiiii <36806487+copyrightpoiiiii@users.noreply.github.com>
2018-10-29 09:15:12 +0800

    Create head.cpp

---
+++ head.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define rep(i,l,r) for(int i=l;i<=r;i++)
+#define per(i,r,l) for(int i=r;i>=l;i--)
+#define for4(i,x) for(int i=head[x],y=e[i].go;i;i=e[i].next,y=e[i].go)
+#define maxn (400000+5)
+#define maxm (400000+5)
+#define mod (1000000007)
+#define ll long long
+#define inf 1000000000
+using namespace std;
+const int dx[4] = {-1, 0, 1, 0};
+const int dy[4] = {0, 1, 0, -1};
+const int dxo[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+const int dyo[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+inline bool is_down(char x) { return ('a' <= x && x <= 'z'); }
+inline bool is_upper(char x) { return ('A' <= x && x <= 'Z'); }
+inline bool is_digit(char x) { return ('0' <= x && x <= '9'); }
+inline int read(){
+    int x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=10*x+ch-'0';ch=getchar();}
+    return x*f;
+}
+inline ll readll(){
+	ll x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=10*x+ch-'0';ch=getchar();}
+    return x*f;
+}
+long long gcd(long long x,long long y){return y?gcd(y,x%y):x;}
+long long power(long long x,long long y){
+	long long t=1;
+	for(;y;y>>=1,x=x*x%mod)
+		if(y&1)t=t*x%mod;
+	return t;
+}
+/*strcut edge{
+    int go,next;
+}e[2*maxm];
+int tot,head[maxn];
+void insert(int u,int v){
+    e[++tot]=(edge){v,head[u]};head[u]=tot;
+    e[++tot]=(edge){u,head[v]}lhead[v]=tot;
+}*/
